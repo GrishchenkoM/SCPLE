7a523c6c40cf9ba34d1429063dcf538c8f0847a6
GrishchenkoM <grishchenko.m@yandex.ru>
2014-07-17 16:38:10 +0300

    Initial Project Commit

---
+++ SCPLE/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SCPLE")]
+[assembly: AssemblyDescription("Specification compiling program based on a list of elements")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hartron-Express LTD")]
+[assembly: AssemblyProduct("SCPLE")]
+[assembly: AssemblyCopyright("Copyright © Hartron-Express LTD 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("106c1cfc-db42-4f3e-9f8b-f35f4bb8940b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
